dd421de717703f0ad9fe16ed6fc2a3755055d6ee
Maris Shapiro <19maris72@gmail.com>
2023-01-22 14:33:16 +0300

    Добавьте файлы проекта.

---
+++ Lab 5 Тема 5 СамРаб Задача5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab_5_Тема_5_СамРаб_Задача5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность N");
+            Console.WriteLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = (i + j) % 2 == 0 ? 1 : 0;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
